527535a5d9240b757ff4ca176399599ad89f3dfd
Brad Chase <brad@webappfromscratch.com>
2015-07-03 18:00:23 -0400

    handle filenames w/ spaces in 2mp3

--- scripts/2mp3.rb
+++ scripts/2mp3.rb
@@ -5,4 +5,4 @@ require 'pathname'
 input  = Pathname.new ARGV[0]
 output = input.basename.to_s.chomp(input.extname) + '.mp3'
 
-%x[avconv -i #{input} -ab 192k #{output}]
+%x[avconv -i #{input.to_s.inspect} -ab 192k #{output.inspect}]
